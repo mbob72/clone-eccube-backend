642d5b4fd37436486e85db965cb1221e880d035e
Dmitrii Mokienko <d.mokienko@maroo.us>
2023-12-07 10:09:42 +0400

    feat: mollie connect

--- src/mollie/mollie.module.ts
+++ src/mollie/mollie.module.ts
@@ -1,9 +1,12 @@
+import { ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { MollieService } from './mollie.service';
 import { MollieController } from './mollie.controller';
 
 @Module({
+  imports: [HttpModule],
   controllers: [MollieController],
-  providers: [MollieService],
+  providers: [MollieService, ConfigService],
 })
 export class MollieModule {}
--- src/mollie/mollie.service.ts
+++ src/mollie/mollie.service.ts
@@ -1,26 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateMollieDto } from './dto/create-mollie.dto';
-import { UpdateMollieDto } from './dto/update-mollie.dto';
 
 @Injectable()
-export class MollieService {
-  create(createMollieDto: CreateMollieDto) {
-    return 'This action adds a new mollie';
-  }
-
-  findAll() {
-    return `This action returns all mollie`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} mollie`;
-  }
-
-  update(id: number, updateMollieDto: UpdateMollieDto) {
-    return `This action updates a #${id} mollie`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} mollie`;
-  }
-}
+export class MollieService {}
